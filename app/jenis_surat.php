8132949153be3754b2106dbc3704d468e9c81b0e
Luis Ismail <luisismail61@gmail.com>
2018-03-28 17:34:50 +0800

    surat keluar

--- app/jenis_surat.php
+++ app/jenis_surat.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class jenis_surat extends Model
 {
-    //
+    protected $table = 'jenis_surat';
+    public $incrementing = false;
+    protected $fillable = [
+
+    	'nama','format_surat','keterangan'
+
+    ];
+   
+
+    public function relasiJenis_surat()
+    {
+        return $this->belongsTo('App\surat_keluar');
+
+        return $this->belongsTo('App\surat_masuk');
+
+    }
 }
