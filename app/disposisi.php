94ec6c403f5755809164efa752610b7caa133b68
Luis Ismail <luisismail61@gmail.com>
2018-03-28 17:41:24 +0800

    disposisi

--- app/disposisi.php
+++ app/disposisi.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class disposisi extends Model
 {
-    //
+    protected $table = 'disposisi';
+
+    protected $fillable = [
+
+    	'keterangan'
+
+    ];
+
+    protected $primaryKey = 'kodeBranch';
+
+    public function relasiDisposisi()
+    {
+    	return $this->hasMany('App\user');
+
+    	return $this->hasMany('App\surat_masuk');
+
+        return $this->hasMany('App\disposisi');
+
+        return $this->belongsTo('App\disposisi');
+
+    }
 }
