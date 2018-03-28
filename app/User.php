764c547ef40e514f8fd3b15e07f3a63211557304
Luis Ismail <luisismail61@gmail.com>
2018-03-28 17:20:33 +0800

    model user

--- app/User.php
+++ app/User.php
@@ -9,24 +9,22 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
      * @var array
      */
     protected $fillable = [
+
         'name', 'email','kontak','alamat','foto'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
+
      * @var array
      */
     protected $hidden = [
+
         'password', 'remember_token',
     ];
     public function queryUser(){
+        
         return $this->hasMany(App\surat_keluar);
     }
 }
