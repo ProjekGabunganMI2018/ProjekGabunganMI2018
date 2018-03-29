73e16485e33a7e54ceeb4bb49a29cc748f340624
Luis Ismail <luisismail61@gmail.com>
2018-03-29 10:59:37 +0800

    surat keluar

--- app/surat_keluar.php
+++ app/surat_keluar.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class surat_keluar extends Model
 {
-    //
+    protected $table = 'branch';
+    public $incrementing = false;
+    protected $fillable = [
+
+    	'nomor_surat','perihal','isi'
+
+    ];
+    
+    protected $primaryKey = 'nomor_surat';
+
+
+    public function relasiSurat_keluar()
+    {
+        return $this->hasMany('App\jenis_surat');
+    }
+
+
+    @return
+
+    public function relasiSurat_keluar2()
+    {
+    	return $this->hasMany('App\user');
+
+    }
+
+
+    @return
+
+    public function relasiSurat_keluar3()
+    {
+    	return $this->hasMany('App/surat_masuk');
+
+    }
+
 }
