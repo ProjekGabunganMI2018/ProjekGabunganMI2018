417e575fe49258f652a9cf5b31a4675cd2c86414
Luis Ismail <luisismail61@gmail.com>
2018-03-31 08:49:05 +0800

    perbaikan relasi

--- app/jenis_surat.php
+++ app/jenis_surat.php
@@ -2,11 +2,20 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Eloquent as model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class jenis_surat extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'jenis_surat';
+
+    const CREATED_AT = 'created_at';
+    
+    const UPDATE_AT = 'update_at';
+    
     protected $fillable = [
 
     	'nama','format_surat','keterangan'
@@ -24,6 +33,6 @@ class jenis_surat extends Model
     
     public function relasiJenis_surat2()
     {
-        return $this->belongsTo('App\surat_masuk');
+        return $this->hasMany('App\surat_masuk');
     }
 }
