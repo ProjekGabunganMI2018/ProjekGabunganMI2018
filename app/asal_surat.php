87a8410916934cec2a9315155e4e29d122325bb6
Luis Ismail <luisismail61@gmail.com>
2018-03-31 08:56:02 +0800

    penambahan soft delete

--- app/asal_surat.php
+++ app/asal_surat.php
@@ -2,12 +2,19 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class asal_surat extends Model
 {
+	use SoftDeletes;
+
     protected $table = 'asal_surat';
 
+    const CREATED_AT = 'created_at';
+
+    const UPDATE_AT = 'update_at';
 
     protected $fillable = [
 
