e5f9ab4c732a3ee63d55ccb919b4b96e93977647
Budi22 <36613051+Budi22@users.noreply.github.com>
2018-04-01 18:22:06 +0800

    Add files via upload

---
+++ homecontroller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homecontroller extends Controller
+{
+    public function home(){
+    	return view ('home');
+    }
+    public function surat(){
+    	return view ('surat');
+    }
+    public function surat(){
+    	return view ('surat');
+    }
+   
+}
