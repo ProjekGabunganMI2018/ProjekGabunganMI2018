6fee832b6462857e3eca213f07f2aca975ace2b6
rufiatin13 <36654904+rufiatin13@users.noreply.github.com>
2018-04-02 17:40:47 +0800

    Add files via upload
    edit user

--- Edit.blade.php
+++ Edit.blade.php
@@ -1,10 +1,10 @@
 <html>
 <body>
 
-<h2>Form surat masuk</h2>
-		<form action="/surat masuk/{{$surat masuk_->id}}" method="POST">
+<h2>Form user</h2>
+		<form action="/user/{{$user_->id}}" method="POST">
 			<input type="hidden" name="_method" value="PATCH"/>
-			@include('surat masuk')
+			@include('user')
 		</form>
 
 </body>
