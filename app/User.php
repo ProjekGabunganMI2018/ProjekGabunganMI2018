c301fef82c696b1d006ff7365c125e92d4511a63
Luis Ismail <luisismail61@gmail.com>
2018-04-02 12:48:54 +0800

    user pindah ke app

--- app/User.php
+++ app/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,21 +11,16 @@ class User extends Authenticatable
 
      * @var array
      */
-    protected $fillable = [
+    protected $fillable = ['name', 'email','kontak','alamat','foto'];
 
-        'name', 'email','kontak','alamat','foto'
-    ];
-
-
-     * @var array
-     */
     protected $hidden = [
 
         'password', 'remember_token',
     ];
-    public function queryUser(){
+
+    public function RelasiUser(){
         
-        return $this->hasMany(App\Models\surat_keluar);
+        return $this->hasMany('App\Models\SuratKeluar');
     }
     
 }
