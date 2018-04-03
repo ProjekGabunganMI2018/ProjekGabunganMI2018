218f0c67325f40ed91e27483559b72a4a6e16ba7
User\Acer <marcelinasulle>
2018-04-03 14:43:47 +0700

    SuratMasukController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -24,14 +24,14 @@ class LoginController extends Controller
      * Where to redirect users after login.
      *
      * @var string
-     */
+
     protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
      *
      * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
---
+++ app/Http/Controllers/SuratKeluarController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Acer
+ * Date: 03/04/2018
+ * Time: 14:42
+ */
