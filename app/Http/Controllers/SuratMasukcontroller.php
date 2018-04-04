1561c0618872ab27d21f43a462167b315f9a948b
marcelinasulle <sullemarcelina@gmail.com>
2018-04-04 20:08:50 +0700

    Merge branch 'master' of https://github.com/ProjekGabunganMI2018/ProjekGabunganMI2018
    # Conflicts:
    #	composer.lock
    #	config/app.php

--- app/Http/Controllers/SuratMasukcontroller.php
+++ app/Http/Controllers/SuratMasukcontroller.php
@@ -1,19 +1,51 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('test', function () {
-    return view('test');
-});
-//Tambahan
-Route::get('/test2', 'testController@test');
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\SuratMasuk;
+class SuratMasukController extends Controller
+{
+    public function create()
+    {
+        return view('asal_surat.create');
+    }
+
+    public function store(Request $request){
+        $input=$request->all();
+
+        SuratMasuk::create($input);
+
+        return redirect('asal_surat');
+    }
+
+    public function index(){
+        $Suratmasuk=Suratmasuk::all();
+        return view('asal_surat.index',compact('list_asalsurat'));
+    }
+
+    public function destroy($id){
+        $suratmasuk=Suratmasuk::where('id',$id)->delete();
+        return redirect('asal_surat');
+    }
+    public function edit($id) {
+        $suratmasuk=Suratmasuk::where('id',$id)->first();
+        return view('asal_surat.edit',compact('asalsurat'));
+
+    }
+    public function update($id, Request $request){
+        $input=$request->except(['_method','_token']);
+
+        $suratmasuk=Suratmasuk::where('id',$id)->first()->update($input);
+
+        return redirect('asal_surat');
+    }
+
+    public function show($id){
+        $suratmasuk=Suratmasuk::where('id',$id)->first();
+
+        return view('asal_surat.show',compact('asalsurat'));
+    }
+
+
+}
