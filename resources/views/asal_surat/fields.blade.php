4fbd7107b4ffd934a94227177d789a661e6e2939
User\Acer <marcelinasulle>
2018-04-04 14:23:28 +0700

    env

--- resources/views/asal_surat/fields.blade.php
+++ resources/views/asal_surat/fields.blade.php
@@ -4,7 +4,13 @@
     {!! Form::text('nama', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Format Surat Field -->
+<!-- Instansi Field -->
+<div class="form-group col-sm-12 col-lg-12">
+    {!! Form::label('instansi', 'Instansi:') !!}
+    {!! Form::textarea('instansi', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Kontak Field -->
 <div class="form-group col-sm-12 col-lg-12">
     {!! Form::label('format_surat', 'Format Surat:') !!}
     {!! Form::textarea('format_surat', null, ['class' => 'form-control']) !!}
