8a75e3b09bee25804fac48207a0b39072319a4b7
Luis Ismail <luisismail61@gmail.com>
2018-04-04 22:31:54 +0800

    jenis surat complete

---
+++ resources/views/jenis_surat/show.blade.php
@@ -0,0 +1,25 @@
+/**
+ * Created by PhpStorm.
+ * User: Luis Ismail
+ * Date: 04/04/2018
+ * Time: 21.48
+ */
+@extends('layouts.app')
+
+@section('content')
+    <section class="content-header">
+        <h1>
+            Asal Surat
+        </h1>
+    </section>
+    <div class="content">
+        <div class="box box-primary">
+            <div class="box-body">
+                <div class="row" style="padding-left: 20px">
+                    @include('jenis_surat.show_fields')
+                    <a href="{!! route('jenis_surat.index') !!}" class="btn btn-default">Back</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
