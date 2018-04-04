550a840af96bacca3b111d8fcf9f23b709d7d46b
Luis Ismail <luisismail61@gmail.com>
2018-04-04 20:37:42 +0800

    disposisi

--- resources/views/Disposisi/Create.blade.php
+++ resources/views/Disposisi/Create.blade.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Rizky Abdika
- * Date: 02/04/2018
- * Time: 14:55
- */
+@extends('layouts.app')
+
+@section('content')
+    <section class="content-header">
+        <h1>
+            Disposisi
+        </h1>
+    </section>
+    <div class="content">
+        <div class="box box-primary">
+
+            <div class="box-body">
+                <div class="row">
+                    {!! Form::open(['route' => 'disposisi.store']) !!}
+
+                    @include('disposisi.fields')
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
