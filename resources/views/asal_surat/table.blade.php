0f7f681e1866dacdc9d91a0299234e0b12dc742e
Luis Ismail <luisismail61@gmail.com>
2018-04-04 19:49:25 +0800

    button delete

--- resources/views/asal_surat/table.blade.php
+++ resources/views/asal_surat/table.blade.php
@@ -22,7 +22,7 @@
                 <div class='btn-group'>
                     <a href="{!! route('asal_surat.show', [$AsalSurat->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
                     <a href="{!! route('asal_surat.edit', [$AsalSurat->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Beneran nih mau dihapus?')"]) !!}
                 </div>
                 {!! Form::close() !!}
             </td>
