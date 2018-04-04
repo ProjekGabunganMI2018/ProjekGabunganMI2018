f2c96d37f049ce1d6280e2608ed504d6e6da7fa6
bobbyauliaananda <ananda.bobby@yahoo.co.id>
2018-04-04 06:06:14 -0700

    edit
    edit JenisSurat

--- resources/views/JenisSurat/Edit.blade.php
+++ resources/views/JenisSurat/Edit.blade.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Rizky Abdika
- * Date: 02/04/2018
- * Time: 14:55
- */
+<html>
+<body>
+
+<h2>Form Jenis Surat</h2>
+<form action="/JenisSurat/{{$list_jenissurat->id}}" method="post" >
+    <input type="hidden" name="_method" value="PATCH"/>
+    @include('JenisSurat.form')
+</form>
+
+</body>
+</html>
