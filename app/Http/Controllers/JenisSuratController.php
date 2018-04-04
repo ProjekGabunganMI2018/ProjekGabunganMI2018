2ac206281b686ad745f56bc6bd4e60f2fdf14a8a
Luis Ismail <luisismail61@gmail.com>
2018-04-04 10:48:30 +0800

    ganti path

--- app/Http/Controllers/JenisSuratController.php
+++ app/Http/Controllers/JenisSuratController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\JenisSurat;
+use App\Models\JenisSurat;
 class JenisSuratController extends Controller
 {
     public function create()
