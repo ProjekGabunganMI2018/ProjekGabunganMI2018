472650355e4601e1410f01a79f5185c626ccd290
Luis Ismail <luisismail61@gmail.com>
2018-04-05 00:38:59 +0800

    hapus file lama (surat masuk)

--- app/Http/Controllers/SuratMasukcontroller.php
+++ app/Http/Controllers/SuratMasukcontroller.php
@@ -1,19 +1,52 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('test', function () {
-    return view('test');
-});
-//Tambahan
-Route::get('/test2', 'testController@test');
+
+namespace App\Http\Controllers;
+
+use App\Models\SuratMasuk;
+use Illuminate\Http\Request;
+
+class SuratMasukController extends Controller
+{
+ public function create()
+    {
+        return view('surat_masuk.create');
+    }
+
+    public function store(Request $request){
+        $input=$request->all();
+
+        SuratMasuk::create($input);
+
+        return redirect('surat_masuk');
+    }
+
+    public function index(){
+        $list_suratmasuk=SuratMasuk::all();
+        return view('surat_masuk.index',compact('list_suratmasuk'));
+    }
+
+    public function destroy($id){
+        $suratmasuk=SuratMasuk::where('id',$id)->delete();
+        return redirect('surat_masuk');
+    }
+    public function edit($id) {
+        $suratmasuk=SuratMasuk::where('id',$id)->first();
+        return view('surat_masuk.edit',compact('suratmasuk'));
+
+    }
+    public function update($id, Request $request){
+        $input=$request->except(['_method','_token']);
+
+        $suratmasuk=SuratMasuk::where('id',$id)->first()->update($input);
+
+        return redirect('surat_masuk');
+    }
+
+    public function show($id){
+        $suratmasuk=SuratMasuk::where('id',$id)->first();
+
+        return view('surat_masuk.show',compact('suratmasuk'));
+    }
+
+
+}
