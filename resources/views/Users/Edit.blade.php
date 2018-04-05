496465fe120c92e5d78b366a71dde5717867afb5
Luis Ismail <luisismail61@gmail.com>
2018-04-06 06:28:53 +0800

    q

---
+++ resources/views/Users/Edit.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="content-header">
+        <h1>
+            User
+        </h1>
+   </section>
+   <div class="content">
+       <div class="box box-primary">
+           <div class="box-body">
+               <div class="row">
+                   {!! Form::model($user, ['route' => ['users.update', $user->id], 'method' => 'patch']) !!}
+
+                        @include('users.fields')
+
+                   {!! Form::close() !!}
+               </div>
+           </div>
+       </div>
+   </div>
+@endsection
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,51 +1,15 @@
-    <li class="{{  Request::is('roles*')||Request::is('permissions*')||Request::is('user_role*')||Request::is('users*') ? 'active' : '' }} treeview">
-        <a href="#">
-            <i class="fa fa-cog"></i> <span>Pengaturan Role & User</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-        </a>
-        <ul class="treeview-menu">
-            <li class="{{ Request::is('users*') ? 'active' : '' }}">
-                <a href="{!! route('users.index') !!}"><i class="fa fa-users"></i><span>Users</span></a>
-            </li>
-            <li class="{{ Request::is('roles*') ? 'active' : '' }}">
-                <a href="{!! route('roles.index') !!}"><i class="fa fa-sun-o"></i><span>Roles</span></a>
-            </li>
-
-            {{-- <li class="{{ Request::is('permissions*') ? 'active' : '' }}">
-                <a href="{!! route('permissions.index') !!}"><i class="fa fa-spinner"></i><span>Permissions</span></a>
-            </li> --}}
-
-            <li class="{{ Request::is('user_role*') ? 'active' : '' }}">
-                <a href="{!! route('user_role.index') !!}"><i class="fa fa-object-group"></i><span>User Role</span></a>
-            </li>
-        </ul>
-    </li>
-
-    <li class="{{  Request::is('jenisSurats*')||Request::is('asalSurats*') ? 'active' : '' }} treeview">
-        <a href="#">
-            <i class="	fa fa-cubes"></i> <span>Master Data</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-        </a>
-        <ul class="treeview-menu">
-            <li class="{{ Request::is('jenisSurats*') ? 'active' : '' }}">
-                <a href="{!! route('jenisSurats.index') !!}"><i class="fa fa-edit"></i><span>Jenis Surat</span></a>
-            </li>
-
-            <li class="{{ Request::is('asalSurats*') ? 'active' : '' }}">
-                <a href="{!! route('asalSurats.index') !!}"><i class="fa fa-edit"></i><span>Asal Surat</span></a>
-            </li>
-        </ul>
-    </li>
-
-
 <li class="{{ Request::is('surat_masuk*') ? 'active' : '' }}">
     <a href="{!! route('surat_masuk.index') !!}"><i class="fa fa-edit"></i><span>Surat Masuk</span></a>
 </li>
 
 <li class="{{ Request::is('surat_keluar*') ? 'active' : '' }}">
 <a href="{!! route('surat_keluar.index') !!}"><i class="fa fa-edit"></i><span>Surat Keluar</span></a>
+</li>
+
+<li class="{{ Request::is('asal_surat*') ? 'active' : '' }}">
+    <a href="{!! route('asal_surat.index') !!}"><i class="fa fa-edit"></i><span>Asal Surat</span></a>
+</li>
+
+<li class="{{ Request::is('jenis_surat*') ? 'active' : '' }}">
+    <a href="{!! route('jenis_surat.index') !!}"><i class="fa fa-edit"></i><span>Jenis Surat</span></a>
 </li>
