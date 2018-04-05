30e2e1ecd3d0ccb587d2d5dc9845b4c34967a3d6
Luis Ismail <luisismail61@gmail.com>
2018-04-05 11:46:14 +0800

    relasi

--- app/User.php
+++ app/User.php
@@ -21,9 +21,29 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function RelasiUser(){
+    public function pembuat_surat(){
         
-        return $this->hasMany('App\Models\SuratKeluar');
+        return $this->hasMany('App\Models\SuratKeluar','pembuat_surat_id');
+    }
+
+    public function  peminta_surat(){
+        return $this->hasMany('App\Models\SuratKeluar','peminta_surat_id');
+    }
+
+    public function  surat_masuk(){
+        return $this->hasMany('App\Models\SuratMasuk','tujuan_user_id');
+    }
+
+    public function penyimpan_surat(){
+        return $this->hasMany('App\Models\SuratMasuk','penyimpan_surat_id');
+    }
+
+    public function penerima_disposisi(){
+        return $this->hasMany('App\Models\Disposisi','penerima_disposisi_id');
+    }
+
+    public function pembuat_disposisi(){
+        return $this->hasMany('App\Models\Disposisi','pembuat_disposisi_id');
     }
     
 }
