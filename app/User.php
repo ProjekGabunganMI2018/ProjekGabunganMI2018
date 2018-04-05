ab9db68eb4d14c525f2bee323fd9bfa429b1e5bb
Muhammad Rizki Abdika <rabdika6@gmail.com>
2018-04-05 14:37:17 +0800

    tambahin password eror

--- app/User.php
+++ app/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
 
-        'name', 'email','kontak','alamat','foto'
+        'name', 'email','kontak','alamat','foto','password'
     ];
 
     protected $hidden = [
